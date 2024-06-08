53113163aa3c221f649bc42aa838de8cee547b42
Rin-t <rin.t.4921@gmail.com>
2024-06-08 11:21:38 +0900

    Initial Commit

---
+++ PracticeInterstitialAds/PracticeInterstitialAdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeInterstitialAdsApp.swift
+//  PracticeInterstitialAds
+//
+//  Created by Rin on 2024/06/08.
+//
+
+import SwiftUI
+
+@main
+struct PracticeInterstitialAdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
